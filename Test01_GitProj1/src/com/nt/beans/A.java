b5d4eca9587fea48bcaabfc8ad32487d23132923
dev1 <dev1@gmail.com>
2023-07-29 16:48:20 +0530

    hhh

--- Test01_GitProj1/src/com/nt/beans/A.java
+++ Test01_GitProj1/src/com/nt/beans/A.java
@@ -13,15 +13,27 @@ public class A {
 		System.out.println("abc.123");
 
 	}
-	public void m2() {
 
+	public void m2() {
+		
 		System.out.println("dev m1::12345");
 		System.out.println("abc.123");
 		
-
+		
 	}
+
 	public void m4() {
 		System.out.println("m4");
+	}
+
+
+	public void m3() {
+		
+		System.out.println("dev m1::12345");
+		System.out.println("abc.123");
+		System.out.println("A.m3()");
+		
+		
 	}
 
 }
